e45314913c3911d04bedf12a7d27648074b4a977
Ezequiel Rabinovich <erabinovich@gmail.com>
2019-05-03 15:18:58 -0400

    Allow adding deserializers (#5)
    * Allow registering new deserializers
    
    * Bump minor version

--- src/lib/deserializer.ts
+++ src/lib/deserializer.ts
@@ -5,13 +5,23 @@ import { Serializable } from '../interfaces/serializable';
 
 export abstract class Deserializer<T extends Serializable<Serialized>> {
   protected abstract deserializers: Deserializable<T>[];
+  private added: Deserializable<T>[] = [];
 
   deserialize(obj: Serialized): T {
     const deserializer = this.deserializers
+      .concat(this.added)
       .find(deserializer => deserializer.canDeserialize(obj));
     if (!deserializer) {
       throw new DeserializationError(obj);
     }
     return deserializer.deserialize(obj);
+  }
+
+  add(deserializable: Deserializable<T>) {
+    this.added.push(deserializable);
+  }
+
+  clear() {
+    this.added = [];
   }
 }
